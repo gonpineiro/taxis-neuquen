109ee26b2998bcf2b899056b4700f166f11fdc3c
Norbert Stange <n0rb3r7@outlook.com>
2021-07-26 11:37:29 -0300

    Vista de datos del chofer.
    Datos del chofer en tabla y el botón de imprimir habilitación.

---
+++ public/views/choferes/inscripcion.php
@@ -0,0 +1,139 @@
+<?php
+include '../../../app/config/config.php';
+
+if (!isset($_SESSION['usuario'])) {
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . WEBLOGIN);
+    exit();
+}
+
+$usuarioController = new UsuarioController();
+
+
+/* datos de la sesion */
+include('session.php');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../node_modules/bootstrap-select/dist/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="../../../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../../estilos/estilo.css">
+    <title>Administración de Habilitaciones de Transporte Público Neuquén</title>
+</head>
+
+<body>
+    <?PHP
+    include('./components/header.php');
+    ?>
+    <div class="body container" style="margin-bottom: 5em;">
+        <div class="datos-perfil">
+            <div class="card text-center rounded mb-3" style="background-color:white; margin-top: 1.5em;">
+                <div id="contenedorImagen">
+                    <img src='../../estilos/libreta/icono-persona.png' name="fotografia" class="fotografia card-img-top img-fluid" id="fotografia" style="margin: 20px auto; height:200px; width: 200px;">
+                </div>
+                <div class="card-body" style="background-color: white; color: #315891 !important;">
+                    <p>
+                    <h4 id="cardNombre" class="card-title mt-2"><?= "$nombre $apellido" ?></h4>
+                    </p>
+                    <p id="cardDni"><small><i class="fa fa-envelope ml-0"></i>
+                            <bold>Dni: </bold><?= $dni ?>
+                        </small></p>
+                    <p id="cardTelefono"><small>
+                            <bold>Tel:</bold> <?= $celular; ?>
+                        </small></p>
+                    <p id="cardEmail"><small><i class="fa fa-envelope ml-0"></i>
+                            <bold>Email: </bold><?= $email ?>
+                        </small></p>
+                    <p id="cardFechanacimiento"><small><i class="fa fa-envelope ml-0"></i>
+                            <bold>Fecha de Nacimiento: </bold><?= $fechanacimiento ?>
+                        </small></p>
+                    <p id="cardGenero"><small><i class="fa fa-envelope ml-0"></i>
+                            <bold>G&eacute;nero:</bold> <?= $genero ?>
+                        </small></p>
+                </div>
+            </div>
+        </div>
+        <h1 class="titulo mb-5 mt-5">Choferes </h1>
+        <!-- COMIENZO DE TABS/ PESTAÑAS -->
+        <nav>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <a class="nav-link active" id="nav-choferes-tab" data-toggle="tab" href="#nav-choferes" role="tab" aria-controls="nav-choferes" aria-selected="true">Choferes</a>
+            </div>
+        </nav>
+        <div class="tab-content bg-light" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-choferes" role="tabpanel" aria-labelledby="nav-choferes-tab">
+                <div class="container">
+                    <h4 class="pt-3 pb-3">Datos Personales</h4>
+                    <h5></h5>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <table class="table table-sm table-borderless">
+                                <!-- CREAR NUEVOS TR SOLO VISTA CELULAR PARA LAS FILAS QUE TIENEN 4 DATOS Y OCULTAR LAS COLUMNAS 3 Y 4 DE VISTA PC EN CELULAR-->
+                                <tbody>
+                                    <tr class="d-flex" style="background-color:#fff">
+                                        <td class="col-md-2 col-6 text-right">Persona:</td>
+                                        <td class="col-md-10 col-6">CUIL:20-26810704-6</td>
+                                    </tr>
+                                    <tr class="d-flex">
+                                        <td class="col-md-2 col-6 text-right">Nombre:</td>
+                                        <td class="col-md-10 col-6">BAEZA, SANTIAGO ANDRES</td>
+                                    </tr>
+                                    <tr class="d-flex" style="background-color:#fff">
+                                        <td class="col-md-2 col-6 text-right">Nro Conductor:</td>
+                                        <td class="col-md-10 col-6">3816</td>
+                                    </tr>
+                                    <tr class="d-flex">
+                                        <td class="col-md-2 col-3 text-right">Tipo Licencia:</td>
+                                        <td class="col-md-6 col-3">D2-Autom. de serv. de trans. mas 8 plazas</td>
+                                        <td class="col-md-2 col-3 text-right">Vencimiento Licencia:</td>
+                                        <td class="col-md-2 col-3">30/08/2021</td>
+                                    </tr>
+                                    <tr class="d-flex" style="background-color:#fff">
+                                        <td class="col-md-2 col-3 text-right">Otorgada:</td>
+                                        <td class="col-md-6 col-3">14/11/2019</td>
+                                        <td class="col-md-2 col-3 text-right">Vencimiento:</td>
+                                        <td class="col-md-2 col-3">12/11/2020</td>
+                                    </tr>
+                                    <tr class="d-flex">
+                                        <td class="col-md-2 col-3 text-right">Tipo cambio:</td>
+                                        <td class="col-md-6 col-3">Renovación</td>
+                                        <td class="col-md-2 col-3 text-right">Último cambio:</td>
+                                        <td class="col-md-2 col-3">14/11/2019</td>
+                                    </tr>
+                                    <tr class="d-flex" style="background-color:#fff">
+                                        <td class="col-md-2 col-6 text-right">Observaciones:</td>
+                                        <td class="col-md-10 col-6">CREDENCIAL DE TAXO OTORGADA HASTA 12/11/2020</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end pb-3">
+                                <div class="buttonsRow">
+                                    <button class="btn btn-primary submitBtn" onclick="alert('imprimir la habilitación');">Imprimir Habilitación</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<script src="../../../node_modules/jquery/dist/jquery.min.js"></script>
+<script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../../../node_modules/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+<script src="../../js/formularios/inscripcion.js"></script>
+
+</html>
--- public/views/menu/index.php
+++ public/views/menu/index.php
@@ -22,7 +22,7 @@ $apellido = $nombreapellido[0];
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>Administración de Habilitaciones de Transporte Público Neuquén</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css">
@@ -33,7 +33,6 @@ $apellido = $nombreapellido[0];
     <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-
     <!-- <tbody>
         <tr>
             <td style="padding: 35px;">
@@ -87,8 +86,8 @@ $apellido = $nombreapellido[0];
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#00B4E6" class="bi bi-emoji-frown-fill" viewBox="0 0 16 16">
-                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm-2.715 5.933a.5.5 0 0 1-.183-.683A4.498 4.498 0 0 1 8 9.5a4.5 4.5 0 0 1 3.898 2.25.5.5 0 0 1-.866.5A3.498 3.498 0 0 0 8 10.5a3.498 3.498 0 0 0-3.032 1.75.5.5 0 0 1-.683.183zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#00B4E6" class="bi bi-emoji-smile-fill" viewBox="0 0 16 16">
+                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zM4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z" />
                             </svg>
                             <span style="color: #00B4E6;font-weight:600;"><?php echo "$apellido $nombre"; ?></span>
                         </a>
@@ -105,7 +104,7 @@ $apellido = $nombreapellido[0];
     <div class="container pt-5">
         <div class="row pt-5">
             <div class='col-sm d-flex justify-content-center'>
-                <div class='opcion' onclick='window.location.href = "../formularios/inscripcion.php"'>
+                <div class='opcion' onclick='window.location.href = "../choferes/inscripcion.php"'>
                     <table style='width: 100%;'>
                         <tbody>
                             <tr>
@@ -114,7 +113,21 @@ $apellido = $nombreapellido[0];
                                 </td>
                             </tr>
                             <tr style='height: 60px;'>
-                                <td class='opcion-titulo text-center'>REGISTRO DE PROFESIONALES</td>
+                                <td class='opcion-titulo text-center'>TAXIS/ REMISES</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class='opcion' onclick='window.location.href = "../choferes/inscripcion.php"'>
+                    <table style='width: 100%;'>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class='icono' style='background-image: url(../../estilos/menu/icono-admin.png);'></div>
+                                </td>
+                            </tr>
+                            <tr style='height: 60px;'>
+                                <td class='opcion-titulo text-center'>CHOFERES</td>
                             </tr>
                         </tbody>
                     </table>
