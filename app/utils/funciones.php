87284e351a8a3514033b132888ce5a84ecd4c226
Gonzalo <gon.pineiro@gmail.com>
2021-07-27 13:37:55 -0300

    :fire: Remove Function

--- app/utils/funciones.php
+++ app/utils/funciones.php
@@ -323,28 +323,3 @@ function mostrarError($tipo, $detalle = null)
         return "Hubo un error en la carga de los archivos, porfavor intente nuevamente mas tarde.";
     }
 }
-
-function callWebService($url, $params)
-{
-    try {
-        $params= ['patente' => 'OPC-656', 'action' => 0];
-        $headers = ['Content-type: application/json'];
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://weblogin.muninqn.gov.ar/api/mnqn',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_HTTPHEADER => $headers,
-            CURLOPT_POSTFIELDS => json_encode($params),
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($response, true);
-    } catch (Exception $e) {
-        return $e;
-    }
-}
