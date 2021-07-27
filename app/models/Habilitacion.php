17d8616b16b4fa608237a86e3d184289835133ad
Gonzalo <gon.pineiro@gmail.com>
2021-07-27 12:31:16 -0300

    :HabiÃlÃitacion:

--- app/models/Habilitacion.php
+++ app/models/Habilitacion.php
@@ -2,62 +2,12 @@
 
 /**
  * This is the model class for table "Habilitaciones".
+ *  @property string $url
  */
-class Habilitacion
+class Habilitacion extends BaseController
 {
-    public function save()
+    public static function list($params)
     {
-        $array = json_decode(json_encode($this), true);
-        $conn = new BaseDatos();
-        $result = $conn->store(HABILITACIONES, $array);
-
-        /* Guardamos los errores */
-        if ($conn->getError()) {
-            $error =  $conn->getError() . ' | Error al guardar un habilitacion';
-            cargarLog(null, $this->id_solicitud, $error, get_class(), __FUNCTION__);
-        }
-        return $result;
-    }
-
-    public static function list($param = [], $ops = [])
-    {
-        $conn = new BaseDatos();
-        $habilitacion = $conn->search(HABILITACIONES, $param, $ops);
-
-        /* Guardamos los errores */
-        if ($conn->getError()) {
-            $error =  $conn->getError() . ' | Error al listar las habilitaciones';
-            cargarLog(null, null, $error, get_class(), __FUNCTION__);
-        }
-        $array = [];
-        while ($row = odbc_fetch_array($habilitacion)) array_push($array, $row);
-        return json_encode($array);
-    }
-
-    public static function get($params)
-    {
-        $conn = new BaseDatos();
-        $result = $conn->search(HABILITACIONES, $params);
-        $habilitacion = $conn->fetch_assoc($result);
-
-        /* Guardamos los errores */
-        if ($conn->getError()) {
-            $error =  $conn->getError() . ' | Error a obtener el habilitacion: ' . $params[0];
-            cargarLog(null, null, $error, get_class(), __FUNCTION__);
-        }
-        return json_encode($habilitacion);
-    }
-
-    public static function update($res, $id)
-    {
-        $conn = new BaseDatos();
-        $result = $conn->update(HABILITACIONES, $res, $id);
-
-        /* Guardamos los errores */
-        if ($conn->getError()) {
-            $error =  $conn->getError() . ' | Error a modificar el habilitacion ' . $id;
-            cargarLog(null, null, $error, get_class(), __FUNCTION__);
-        }
-        return $result;
+        return self::callWebService($params);
     }
 }
