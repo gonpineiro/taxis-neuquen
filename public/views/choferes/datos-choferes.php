e06309b0f53aea8392545eccc79479a67a83f232
Norbert Stange <n0rb3r7@outlook.com>
2021-07-27 12:41:36 -0300

    Actualización de función imprimirHabilitacionChofer().
    - Se agregan los id en la vista del front.

--- public/views/choferes/datos-choferes.php
+++ public/views/choferes/datos-choferes.php
@@ -57,65 +57,93 @@ include('../common/session.php');
             </div>
         </div>
         <h1 class="titulo mb-5 mt-5">Choferes </h1>
+        <!-- BÚSQUEDA DEL VEHÍCULO -->
+        <div class="pt-2">
+            <div class="row">
+                <div class="form-group col">
+                    <label for="nro-conductor">Número Conductor </label>
+                    <input type="number" id="nro-conductor" class="form-control" placeholder="3816" name="nro-conductor" required>
+                    <div class="invalid-feedback">
+                        <strong>
+                            Por favor ingrese la patente correctamente.
+                        </strong>
+                    </div>
+                </div>
+                <div class="form-group col">
+                    <label for="fecha_hasta font-weight-bold" style="visibility: hidden;">Buscar </label>
+                    <input onclick="buscarConductor()" type="submit" value="Buscar Conductor" id="buscar-conductor" class="form-control btn-primary" name="buscar-conductor" required style="background-color: #60C1DE;color:white;">
+                </div>
+            </div>
+        </div>
         <!-- COMIENZO DE TABS/ PESTAÑAS -->
         <nav>
             <div class="nav nav-tabs" id="nav-tab" role="tablist">
                 <a class="nav-link active" id="nav-choferes-tab" data-toggle="tab" href="#nav-choferes" role="tab" aria-controls="nav-choferes" aria-selected="true">Choferes</a>
             </div>
         </nav>
-        <div class="tab-content bg-light" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-choferes" role="tabpanel" aria-labelledby="nav-choferes-tab">
-                <div class="container">
-                    <h4 class="pt-3 pb-3">Datos Personales</h4>
-                    <h5></h5>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <table class="table table-sm table-borderless">
-                                <!-- CREAR NUEVOS TR SOLO VISTA CELULAR PARA LAS FILAS QUE TIENEN 4 DATOS Y OCULTAR LAS COLUMNAS 3 Y 4 DE VISTA PC EN CELULAR-->
-                                <tbody>
-                                    <tr class="d-flex" style="background-color:#fff">
-                                        <td class="col-md-2 col-6 text-right">Persona:</td>
-                                        <td class="col-md-10 col-6">CUIL:20-26810704-6</td>
-                                    </tr>
-                                    <tr class="d-flex">
-                                        <td class="col-md-2 col-6 text-right">Nombre:</td>
-                                        <td class="col-md-10 col-6">BAEZA, SANTIAGO ANDRES</td>
-                                    </tr>
-                                    <tr class="d-flex" style="background-color:#fff">
-                                        <td class="col-md-2 col-6 text-right">Nro Conductor:</td>
-                                        <td class="col-md-10 col-6">3816</td>
-                                    </tr>
-                                    <tr class="d-flex">
-                                        <td class="col-md-2 col-3 text-right">Tipo Licencia:</td>
-                                        <td class="col-md-6 col-3">D2-Autom. de serv. de trans. mas 8 plazas</td>
-                                        <td class="col-md-2 col-3 text-right">Vencimiento Licencia:</td>
-                                        <td class="col-md-2 col-3">30/08/2021</td>
-                                    </tr>
-                                    <tr class="d-flex" style="background-color:#fff">
-                                        <td class="col-md-2 col-3 text-right">Otorgada:</td>
-                                        <td class="col-md-6 col-3">14/11/2019</td>
-                                        <td class="col-md-2 col-3 text-right">Vencimiento:</td>
-                                        <td class="col-md-2 col-3">12/11/2020</td>
-                                    </tr>
-                                    <tr class="d-flex">
-                                        <td class="col-md-2 col-3 text-right">Tipo cambio:</td>
-                                        <td class="col-md-6 col-3">Renovación</td>
-                                        <td class="col-md-2 col-3 text-right">Último cambio:</td>
-                                        <td class="col-md-2 col-3">14/11/2019</td>
-                                    </tr>
-                                    <tr class="d-flex" style="background-color:#fff">
-                                        <td class="col-md-2 col-6 text-right">Observaciones:</td>
-                                        <td class="col-md-10 col-6">CREDENCIAL DE TAXO OTORGADA HASTA 12/11/2020</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+        <div id="sin-datos">
+            <div class="card-body text-center">
+                <span>Ingrese y busque por número de conductor para obtener la información de este. </span>
+            </div>
+        </div>
+        <div id="datos-conductor" style="display: none;">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="text-light">Datos Personales</h4>
+                </div>
+            </div>
+            <div class="tab-content bg-light" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-choferes" role="tabpanel" aria-labelledby="nav-choferes-tab">
+                    <div class="container">
+                        <div class="row pt-5">
+                            <div class="col-lg-12">
+                                <table class="table table-sm table-borderless">
+                                    <!-- CREAR NUEVOS TR SOLO VISTA CELULAR PARA LAS FILAS QUE TIENEN 4 DATOS Y OCULTAR LAS COLUMNAS 3 Y 4 DE VISTA PC EN CELULAR-->
+                                    <tbody>
+                                        <tr class="d-flex" style="background-color:#fff">
+                                            <td class="col-md-2 col-6 text-right">Persona:</td>
+                                            <td class="col-md-10 col-6" id="(ind_identificacion">CUIL: 20-26810704-6</td>
+                                        </tr>
+                                        <tr class="d-flex">
+                                            <td class="col-md-2 col-6 text-right">Nombre:</td>
+                                            <td class="col-md-10 col-6" id="nombrec">BAEZA, SANTIAGO ANDRES</td>
+                                        </tr>
+                                        <tr class="d-flex" style="background-color:#fff">
+                                            <td class="col-md-2 col-6 text-right">Nro Conductor:</td>
+                                            <td class="col-md-10 col-6" id="nro_conductor">3816</td>
+                                        </tr>
+                                        <tr class="d-flex">
+                                            <td class="col-md-2 col-3 text-right">Tipo Licencia:</td>
+                                            <td class="col-md-6 col-3" id="descripcion_lic">D2-Autom. de serv. de trans. mas 8 plazas</td>
+                                            <td class="col-md-2 col-3 text-right">Vencimiento Licencia:</td>
+                                            <td class="col-md-2 col-3" id="fecha_vencimiento_licencia">30/08/2021</td>
+                                        </tr>
+                                        <tr class="d-flex" style="background-color:#fff">
+                                            <td class="col-md-2 col-3 text-right">Otorgada:</td>
+                                            <td class="col-md-6 col-3" id="fecha_otorgada">14/11/2019</td>
+                                            <td class="col-md-2 col-3 text-right">Vencimiento:</td>
+                                            <td class="col-md-2 col-3" id="fecha_vencimiento">12/11/2020</td>
+                                        </tr>
+                                        <tr class="d-flex">
+                                            <td class="col-md-2 col-3 text-right">Tipo cambio:</td>
+                                            <td class="col-md-6 col-3" id="tipo_cambio">Renovación</td>
+                                            <td class="col-md-2 col-3 text-right">Último cambio:</td>
+                                            <td class="col-md-2 col-3" id="fecha_ultimo_cambio">14/11/2019</td>
+                                        </tr>
+                                        <tr class="d-flex" style="background-color:#fff">
+                                            <td class="col-md-2 col-6 text-right">Observaciones:</td>
+                                            <td class="col-md-10 col-6" id="observaciones">CREDENCIAL DE TAXI OTORGADA HASTA 12/11/2020</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="d-grid gap-2 d-md-flex justify-content-md-end pb-3">
-                                <div class="buttonsRow">
-                                    <button class="btn btn-primary submitBtn" onclick="imprimirHabilitacionChofer()">Imprimir Habilitación</button>
+                        <div class="row">
+                            <div class="col">
+                                <div class="d-grid gap-2 d-md-flex justify-content-md-end pb-3">
+                                    <div class="buttonsRow">
+                                        <button class="btn btn-primary submitBtn" onclick="imprimirHabilitacionChofer()">Imprimir Habilitación</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
