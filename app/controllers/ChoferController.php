1f80db2b093c88aeaad812207af59bda20e0d280
Gonzalo <gon.pineiro@gmail.com>
2021-07-28 12:11:53 -0300

    :hammer: Cambio nombre del metodo

--- app/controllers/ChoferController.php
+++ app/controllers/ChoferController.php
@@ -3,15 +3,9 @@
 class ChoferController
 {
     /* Busca todas las choferes */
-    public function index($param)
+    public function get($param)
     {
         $chofer = new Chofer();
         return $chofer->list($param);
-    }
-
-    /* Busca una chofer */
-    public static function get($params)
-    {
-        // return Habilitacion::get($params);
     }
 }
