723578aefb48953c683cfe48205e5e4c661c4dec
Gonzalo <gon.pineiro@gmail.com>
2021-07-28 12:08:57 -0300

    :hammer: Add Chofer Model

---
+++ app/models/Chofer.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This is the model class for table "Choferes".
+ */
+class Chofer extends Base
+{
+    public function list(int $conductorID)
+    {
+        $params = ['action' => 1, 'conductorID' => $conductorID];
+        return $this->callWebService($params)[0];
+    }
+}
