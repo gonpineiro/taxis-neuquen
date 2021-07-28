8194873e41cd31bfbd02febcdd4026bf03cdc5c0
Gonzalo <gon.pineiro@gmail.com>
2021-07-28 12:12:07 -0300

    :hammer: Cambio nombre del metodo

--- app/controllers/HabilitacionController.php
+++ app/controllers/HabilitacionController.php
@@ -3,15 +3,9 @@
 class HabilitacionController
 {
     /* Busca todas las habilitaciones */
-    public function index($param)
+    public function get($param)
     {
         $habilitacion = new Habilitacion();
         return $habilitacion->list($param);
-    }
-
-    /* Busca una habilitacion */
-    public static function get($params)
-    {
-        // return Habilitacion::get($params);
     }
 }
