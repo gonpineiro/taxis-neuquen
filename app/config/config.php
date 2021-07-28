5dab1e9b06fa663b8072df1123712b1764f51d58
Gonzalo <gon.pineiro@gmail.com>
2021-07-28 12:22:27 -0300

    :fire: SetConfig

--- app/config/config.php
+++ app/config/config.php
@@ -32,11 +32,9 @@ define('DB_NAME',  PROD ? $_ENV['DB_NAME'] : 'infoprueba');
 define('DB_PORT',  PROD ? $_ENV['DB_PORT'] : '3306');
 define('DB_CHARSET',  PROD ? $_ENV['DB_CHARSET'] : 'utf8');
 
+/* Configuracion API */
 define('API_URL',  PROD ? $_ENV['API_URL'] : $_ENV['API_URL']);
 define('API_TOKEN',  PROD ? $_ENV['API_TOKEN'] : $_ENV['API_TOKEN']);
-
-/* Configuracion del path fIle */
-define('PATH_FILE_LOCAL', $_ENV['PATH_FILE_LOCAL'] == 'true' ? true : false);
 
 /* Configuración de tablas */
 define('USUARIOS', $_ENV['DB_USUARIOS_TABLE']);
