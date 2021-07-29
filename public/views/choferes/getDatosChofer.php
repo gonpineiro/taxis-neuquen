f1b14f89806d32af6590432c5bf12a808ac53efd
Gonzalo <gon.pineiro@gmail.com>
2021-07-29 10:12:53 -0300

    :hammer: getDatosChofer

--- public/views/choferes/getDatosChofer.php
+++ public/views/choferes/getDatosChofer.php
@@ -1,10 +1,18 @@
 <?php
+include '../../../app/config/config.php';
+
+if (!isset($_SESSION['usuario'])) {
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . WEBLOGIN);
+    exit();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header("HTTP/1.1 200 OK");
     $dato = json_decode(file_get_contents('php://input'));
-
-
-    echo (json_encode($dato));
+    $choferController = new ChoferController();
+    $chofer = $choferController->get($dato->id)[0];
+    echo (json_encode($chofer));
     exit();
 } else {
     header("HTTP/1.1 404");
