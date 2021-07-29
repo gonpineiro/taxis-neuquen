bb84b3e78048e99b7755bea18e94d4eb81c72317
Gonzalo <gon.pineiro@gmail.com>
2021-07-29 11:20:43 -0300

    :hammer: FormatDate

--- app/models/Chofer.php
+++ app/models/Chofer.php
@@ -12,10 +12,23 @@ class Chofer extends Base
         $params = ['action' => 1, 'conductorID' => $conductorID];
         $this->chofer = $this->callWebService($params);
         $this->extractDoc($this->chofer[0]['conductorIdentificacion']);
+        $this->formatDate();
     }
 
     public function getChofer()
     {
         return $this->chofer;
+    }
+
+    private function formatDate()
+    {
+        $timestamp = strtotime($this->chofer[0]['fechaOtorgamiento']);
+        $this->chofer[0]['fechaOtorgamiento'] = date('d/m/Y', $timestamp);
+
+        $timestamp = strtotime($this->chofer[0]["fechaVencimiento"]);
+        $this->chofer[0]['fechaVencimiento'] = date('d/m/Y', $timestamp);
+
+        $timestamp = strtotime($this->chofer[0]["fechaVencimientoLicencia"]);
+        $this->chofer[0]["fechaVencimientoLicencia"] = date('d/m/Y', $timestamp);
     }
 }
