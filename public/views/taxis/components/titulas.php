c0fa627a613bae4e36a06922768adb5121d35eac
Gonzalo <gon.pineiro@gmail.com>
2021-07-30 11:09:54 -0300

    :hammer: Titulas

--- public/views/taxis/components/titulas.php
+++ public/views/taxis/components/titulas.php
@@ -24,12 +24,18 @@
     </div>
     <div class="row">
         <div class="form-group col-xs-12 col-md-6">
-            <label>Categ. Tributaria</label>
-            <input type="text" class="form-control" id="cat-tributaria" readonly />
+            <label>Ingresos Brutos</label>
+            <input type="text" class="form-control" id="ing-brutos" readonly />
         </div>
         <div class="form-group col-xs-12 col-md-6">
-            <label>Ident. Tributaria</label>
-            <input type="text" class="form-control" id="ident-tributaria" readonly />
+            <label>Imp. Ganancias</label>
+            <input type="text" class="form-control" id="imp-ganancias" readonly />
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group col-xs-12 col-md-2">
+            <label>Cant. Cotitulares</label>
+            <input type="text" class="form-control" id="cant-cotitulares" readonly />
         </div>
     </div>
 </div>
