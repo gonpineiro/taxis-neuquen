0f365a255a4eb0992a9eb58615c93f599c9cc037
Gonzalo <gon.pineiro@gmail.com>
2021-07-30 11:09:44 -0300

    :hammer: TryCatch

--- public/views/taxis/datos-taxis.js
+++ public/views/taxis/datos-taxis.js
@@ -10,6 +10,17 @@ async function buscarConductor() {
   d.getElementById("parada").value = "";
   d.getElementById("motivo").value = "";
 
+  /* Reset de los datos - Titulas Responsable */
+  d.getElementById("identificacion").value = "";
+  d.getElementById("nombre").value = "";
+  d.getElementById("cat-tributaria").value = "";
+  d.getElementById("ident-tributaria").value = "";
+  d.getElementById("cant-cotitulares").value = "";
+  d.getElementById("ing-brutos").value = "";
+  d.getElementById("imp-ganancias").value = "";
+  d.getElementById("parada").value = "";
+  d.getElementById("motivo").value = "";
+
   /* fetch de los datos */
   const patente = d.getElementById("patente").value;
   const response = await fetch("./getDatosTaxi.php", {
@@ -17,21 +28,40 @@ async function buscarConductor() {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ id: patente }),
   });
-  const taxi = await response.json();
-  console.log(taxi);
 
-  /* Mostramos la vista */
-  d.getElementById("nav-tabContent").style.display = "block";
-  d.getElementById("nav-tabDescription").style.display = "block";
-  d.getElementById("transporte-publico").style.display = "block";
-  d.getElementById("sin-datos").style.display = "none";
+  try {
+    const taxi = await response.json();
+    /* Mostramos la vista */
+    d.getElementById("nav-tabContent").style.display = "block";
+    d.getElementById("nav-tabDescription").style.display = "block";
+    d.getElementById("transporte-publico").style.display = "block";
+    d.getElementById("sin-datos").style.display = "none";
 
-  /* Insertamos los datos - Transporte Público */
-  d.getElementById("tipo-hab").value = taxi.habTipo;
-  d.getElementById("nro-hab").value = taxi.habNumero;
-  d.getElementById("estado-hab").value = "NO HAY DATOS";
-  d.getElementById("nro-empresa").value = taxi.habNumero;
-  d.getElementById("fecha-baja").value = "NO HAY DATOS";
-  d.getElementById("parada").value = "NO HAY DATOS";
-  d.getElementById("motivo").value = "NO HAY DATOS";
+    /* Insertamos los datos - Transporte Público */
+    d.getElementById("tipo-hab").value = taxi.habTipo;
+    d.getElementById("nro-hab").value = taxi.habNumero;
+    d.getElementById("estado-hab").value = "NO HAY DATOS";
+    d.getElementById("nro-empresa").value = taxi.habNumero;
+    d.getElementById("fecha-baja").value = "NO HAY DATOS";
+    d.getElementById("parada").value = "NO HAY DATOS";
+    d.getElementById("motivo").value = "NO HAY DATOS";
+
+    /* Insertamos los datos - Titulas Responsable */
+    d.getElementById("identificacion").value = taxi.titularEmpresa;
+    d.getElementById("nombre").value = taxi.titularIdentificacion;
+    d.getElementById("cat-tributaria").value = "NO HAY DATOS";
+    d.getElementById("ident-tributaria").value = "NO HAY DATOS";
+    d.getElementById("cant-cotitulares").value = "NO HAY DATOS";
+    d.getElementById("ing-brutos").value = "NO HAY DATOS";
+    d.getElementById("imp-ganancias").value = "NO HAY DATOS";
+    d.getElementById("parada").value = "NO HAY DATOS";
+    d.getElementById("motivo").value = "NO HAY DATOS";
+  } catch (error) {
+    d.getElementById("nav-tabContent").style.display = "none";
+    d.getElementById("nav-tabDescription").style.display = "none";
+    d.getElementById("transporte-publico").style.display = "none";
+    d.getElementById("sin-datos").style.display = "block";
+    d.getElementById("sin-datos-descrip").textContent =
+      "No se encuentra la patente: " + patente;
+  }
 }
