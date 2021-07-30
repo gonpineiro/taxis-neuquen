707be9662f7924ab2b10f1bf2ec861bcabb47a88
Gonzalo <gon.pineiro@gmail.com>
2021-07-30 10:21:05 -0300

    :art: Estructura de archivos/carpetas

---
+++ public/views/taxis/transportePublico.php
@@ -0,0 +1,37 @@
+<div class="container" id="transporte-publico" style="display: none">
+    <h4 class="pt-3 pb-3">Transporte Público</h4>
+    <div class="row">
+        <div class="form-group col-xs-12 col-md-4">
+            <label>Tipo Habilitación</label>
+            <input type="text" class="form-control" id="tipo-hab" readonly />
+        </div>
+        <div class="form-group col-xs-12 col-md-4">
+            <label>Tipo Habilitación</label>
+            <input type="text" class="form-control" id="nro-hab" readonly />
+        </div>
+        <div class="form-group col-xs-12 col-md-4">
+            <label>Estado</label>
+            <input type="text" class="form-control" id="estado-hab" readonly />
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group col-xs-12 col-md-4">
+            <label>Nro Empresa</label>
+            <input type="text" class="form-control" id="nro-empresa" readonly />
+        </div>
+        <div class="form-group col-xs-12 col-md-4" style="margin-left: auto;">
+            <label>Fecha Baja</label>
+            <input type="text" class="form-control" id="fecha-baja" readonly />
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group col-xs-12 col-md-8">
+            <label>Parada</label>
+            <input type="text" class="form-control" id="parada" readonly />
+        </div>
+        <div class="form-group col-xs-12 col-md-4">
+            <label>Motivo</label>
+            <input type="text" class="form-control" id="motivo" readonly />
+        </div>
+    </div>
+</div>
