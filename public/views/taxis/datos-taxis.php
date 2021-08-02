126639894512b74287faa260672578ac625a1b2e
Gonzalo <gon.pineiro@gmail.com>
2021-08-02 10:55:53 -0300

    :hammer: GetPdf

--- public/views/taxis/datos-taxis.php
+++ public/views/taxis/datos-taxis.php
@@ -74,7 +74,7 @@ include('../common/session.php');
             </div>
             <div class="form-group col">
                 <label for="fecha_hasta font-weight-bold" style="visibility: hidden;">Buscar </label>
-                <input onclick="buscarConductor()" type="submit" value="Buscar Vehículo" id="buscar-conductor" class="form-control btn-primary" name="buscar-conductor" required style="background-color: #60C1DE;color:white;">
+                <input onclick="buscarConductor();" type="submit" value="Buscar Vehículo" id="buscar-conductor" class="form-control btn-primary" name="buscar-conductor" required style="background-color: #60C1DE;color:white;">
 
             </div>
         </div>
@@ -123,6 +123,6 @@ include('../common/session.php');
 <script src="../../../node_modules/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js"></script>
-<script src="datos-taxis.js"></script>
+<script src="./datos-taxis.js"></script>
 
 </html>
