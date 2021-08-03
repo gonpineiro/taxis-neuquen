10433969c7ffb1f73b01a9ed6856c086ab2a9f23
Gonzalo <gon.pineiro@gmail.com>
2021-08-03 09:28:40 -0300

    :hammer: Estructura del codigo

--- public/views/taxis/datos-taxis.php
+++ public/views/taxis/datos-taxis.php
@@ -19,15 +19,7 @@ include('../common/session.php');
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../../node_modules/bootstrap-select/dist/css/bootstrap-select.min.css">
-    <link rel="stylesheet" href="../../../node_modules/bootstrap-icons/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../../estilos/estilo.css">
-    <title>Administración de Habilitaciones de Transporte Público Neuquén</title>
-</head>
+<?php include('../common/head.php'); ?>
 
 <body>
     <?php include('../common/header.php'); ?>
