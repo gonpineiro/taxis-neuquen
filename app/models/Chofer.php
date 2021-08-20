40395203532b9eed245d7b224a3055ef31d8504b
Gonzalo <gon.pineiro@gmail.com>
2021-08-20 08:39:57 -0300

    :hammer: Refactor llamada API LIC

--- app/models/Chofer.php
+++ app/models/Chofer.php
@@ -46,78 +46,47 @@ class Chofer extends Base
 
     public function getDatosLicencia()
     {
-        $licencia = $this->getDatosLicenciaApi();
+        $obs = [
+            'cola_api' => "Licencia en cola de consulta, intente nuevamente!",
+            'documento_ine' => "No hay registros con ese número de DNI",
+            'api_licencia' => 'error_api_licencia'
+        ];
 
-        if ($licencia == $this->errores['cola_api']) return $this->errores['api_licencia'];
-        if ($licencia == $this->errores['documento_ine']) return $this->errores['documento_ine'];
-
-        /* Filtramos el arreglo para traiga a la persona correspondiente */
-        $licencia = array_filter($licencia, function ($array) {
-            $apellidoLicencia = explode(',', $array['razonSocial'])[0];
-            $apellidoChofer = explode(',', $this->chofer["conductorRazonSocial"])[0];
-            return $apellidoLicencia == $apellidoChofer;
-        })[0]['licencia'];
-
-        if (is_array($licencia['subclaseID'])) {
-            $licencia['subclaseID'] = implode(' - ', $licencia['subclaseID']);
-        }
-
-        $timestamp = strtotime($licencia["fechaEmision"]);
-        $licencia["fechaEmision"] = date('d/m/Y', $timestamp);
-
-        $timestamp = strtotime($licencia["fechaVigencia"]);
-        $licencia["fechaVigencia"] = date('d/m/Y', $timestamp);
-
-        return $licencia;
-    }
-
-    private function getDatosLicenciaApi(string $method = 'POST')
-    {
         $params = ['action' => 2, 'documento' => $this->documento_renaper];
-        $intentos = 0;
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => API_URL_LIC,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_HTTPHEADER => $this->headers,
-            CURLOPT_POSTFIELDS => json_encode($params),
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => $method,
-        ));
 
         $intentos = 0;
         do {
-            try {
-                $response = curl_exec($curl);
+            $response = $this->callWebService($params, API_URL_LIC);
 
-                /* Verificamos que no haya error de conexion */
-                if (!is_null(json_decode($response, true)['error'])) {
-                    return $this->errores['api_licencia'];
+            if (!is_null($response['error'])) return $obs['api_licencia'];
+
+            $response = $response['value'];
+
+            if ($response[0]['status'] == $obs['documento_ine']) return $obs['documento_ine'];
+
+            if ($response[0]['status'] == $obs['cola_api']) $intentos++;
+
+            if (is_null($response[0]['status'])) {
+                /* Filtramos el arreglo para traiga a la persona correspondiente */
+                $licencia = array_filter($response, function ($array) {
+                    $apellidoLicencia = explode(',', $array['razonSocial'])[0];
+                    $apellidoChofer = explode(',', $this->chofer["conductorRazonSocial"])[0];
+                    return $apellidoLicencia == $apellidoChofer;
+                })[0]['licencia'];
+
+                if (is_array($licencia['subclaseID'])) {
+                    $licencia['subclaseID'] = implode(' - ', $licencia['subclaseID']);
                 }
 
-                $response = json_decode($response, true)['value'];
-                switch ($response[0]['status']) {
-                    case null:
-                        curl_close($curl);
-                        return $response;
-                        break;
-                    case $this->errores['cola_api']:
-                        $intentos++;
-                        break;
-                    case $this->errores['documento_ine']:
-                        curl_close($curl);
-                        return $this->errores['documento_ine'];
-                        break;
-                }
-            } catch (Exception $e) {
-                curl_close($curl);
-                return $e;
+                $timestamp = strtotime($licencia["fechaEmision"]);
+                $licencia["fechaEmision"] = date('d/m/Y', $timestamp);
+
+                $timestamp = strtotime($licencia["fechaVigencia"]);
+                $licencia["fechaVigencia"] = date('d/m/Y', $timestamp);
+
+                return $licencia;
             }
         } while ($intentos < 3);
-        curl_close($curl);
-        return $this->errores['cola_api'];
+        return $obs['api_licencia'];
     }
 }
