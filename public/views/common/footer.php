a420de1cae251626623120a014e97a42f54c6973
Norbert Stange <n0rb3r7@outlook.com>
2021-09-08 14:53:55 -0300

    Fix del jspdf para la lectura de archivos con adobe acrobat.

--- public/views/common/footer.php
+++ public/views/common/footer.php
@@ -3,7 +3,7 @@
 <script src="../../../node_modules/jquery/dist/jquery.min.js"></script>
 <script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script src="../../../node_modules/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/davidshimjs/qrcodejs/qrcode.min.js"></script>
 
